202e79b8ed53758900c772f66743c09680cf5e76
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-11-05 19:47:00 +0200

    Added tests for Gatherable, Potion, Rarity and Tree

---
+++ src/tests/GatherableTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Gatherable Tests")
+public class GatherableTest {
+    @Nested
+    @DisplayName("Gatherable Methods Tests")
+    class GatherableMethodsTests {
+        @Test
+        @DisplayName("Test Getters")
+        public void testGetters() {
+            Gatherable gatherable = new Gatherable(10, Rarity.COMMON) {};
+            assertEquals(10, gatherable.getQuantity());
+            assertEquals(Rarity.COMMON, gatherable.getRarity());
+        }
+
+        @Test
+        @DisplayName("Test Setters")
+        public void testSetters() {
+            Gatherable gatherable = new Gatherable(10, Rarity.COMMON) {};
+            gatherable.setQuantity(20);
+            gatherable.setRarity(Rarity.RARE);
+            assertEquals(20, gatherable.getQuantity());
+            assertEquals(Rarity.RARE, gatherable.getRarity());
+        }
+    }
+}
