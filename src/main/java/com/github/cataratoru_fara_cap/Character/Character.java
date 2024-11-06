ec990c28962b864bc8f2d2968e4a81e60cd6629c
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-11-06 11:42:05 +0200

    Modified field visibilyty for Character

--- src/main/java/com/github/cataratoru_fara_cap/Character/Character.java
+++ src/main/java/com/github/cataratoru_fara_cap/Character/Character.java
@@ -1,11 +1,11 @@
 package com.github.cataratoru_fara_cap.Character;
 
 public abstract class Character {
-    protected String name;
-    protected boolean isAlive;
-    protected int attack;
-    protected int defense;
-    protected int health;
+    public String name;
+    public boolean isAlive;
+    public int attack;
+    public int defense;
+    public int health;
     
     abstract public void damage(int damage, Character enemy);
     abstract public void takeDamage(int damage, Character enemy);
